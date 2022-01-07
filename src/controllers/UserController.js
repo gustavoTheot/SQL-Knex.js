3b3af70edc2bc0ab15d2b5bcabb7da0bb94731ea
Gustavo Theot <gustavotheotonio46@gmail.com>
2022-01-06 23:08:15 -0300

    CRUD de usuários

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,61 @@
 const db = require('../database') // utilizando coneção com o db
 
 module.exports = {
+    // List
     async index(req, res) {
         const results = await db('users')
 
         return res.json(results)
+    },
+
+    // Create
+    async create(req, res, next){
+        try{
+            const { username } = req.body
+
+            await db('users').insert({
+                username
+            })
+
+            return res.status(201).send()
+
+        }catch (error){
+            next(error)
+        }
+    },
+
+    // Update 
+    async update(req, res, next){
+        try{
+            const { username } = req.body 
+            const { id } = req.params
+
+            await db('users')
+            .update({username})
+            .where({id})
+
+            return res.status(201).send()
+
+        }catch(error){
+            next(error)
+        }
+    },
+
+    // Delete 
+    async delete(req, res, next){
+        try{
+            const {id} = req.params
+
+            let del = await db('users').where({id}).del()
+
+            if(!del){
+                return res.status(404).json({error: 'Nao encontrado'})
+            }
+
+            return res.json({inform: 'Deletado'}).send()
+
+        }catch(error){
+            next(error)
+        }
     }
 }
--- src/server.js
+++ src/server.js
@@ -2,8 +2,22 @@ const express = require('express') // importando express
 const routes = require('./routes') // chamando o caminho de rotas
 
 const app = express()  // utilizando o express
+
+app.use(express.json()) // utilizando json no express
 app.use(routes) // utilizando rotas
 
+// notFound
+app.use((req, res, next) => {
+    const error = new Error(`Not found`)
+    error.status = 404
+    next(error)
+})
+
+// cactch all
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 app.listen(3333, () => console.log('Server is running')) // ouve a porta e depois roda um função
 
